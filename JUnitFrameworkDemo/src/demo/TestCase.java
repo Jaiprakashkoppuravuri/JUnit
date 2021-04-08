3025e8014a858d09f0cbfefa72eaa6580f542854
Jaiprakashkoppuravuri <73999987+Jaiprakashkoppuravuri@users.noreply.github.com>
2021-04-08 15:03:52 +0530

    commit

---
+++ JUnitFrameworkDemo/src/demo/TestCase.java
@@ -0,0 +1,50 @@
+package demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestCase {
+	
+	@BeforeClass
+	public static void beforeclassmethod()
+	{
+		System.out.println("Before class");
+	}
+	@Before
+	public void beforetestcase()
+	{
+		System.out.println("Before Testcase");
+		
+	}
+	@Test
+	public void test() 
+	{
+		//System.out.println("Your actual Test case");
+		assertEquals(4, FindResult.FindMaxNum(new int [] {1,2,3,4}));
+		assertEquals(-1, FindResult.FindMaxNum(new int [] {-1,-2,-3,-4}));
+		assertEquals(0, FindResult.FindMaxNum(new int [] {1,2,3,4}));
+		
+		assertTrue(FindResult.isEvenNum(2));
+		assertTrue(FindResult.isEvenNum(4));
+		assertTrue(FindResult.isEvenNum(3));
+		
+		assertFalse(FindResult.isEvenNum(3));
+		assertFalse(FindResult.isEvenNum(4));
+	}
+	@After
+	public void aftertestcase()
+	{
+		System.out.println("After Test Case");
+	}
+	@AfterClass
+	public static void afterclass()
+	{
+		System.out.println("After  Class");
+	}
+
+}
